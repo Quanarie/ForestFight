c9e6477f6e81354142ef97ec1166eb76fdef3820
sppeeedd <qustaine@gmail.com>
2021-06-17 00:37:45 +0300

    Added player idle animation
    idleanim

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private Joystick joystick;
 
+    private bool isFacingRight = true;
+
     private void Update()
     {
         float xAxis = joystick.Horizontal;
@@ -15,6 +17,17 @@ public class PlayerMovement : MonoBehaviour
         Vector3 pos = transform.position;
         pos.x += xAxis * Time.deltaTime * speed;
         pos.y += yAxis * Time.deltaTime * speed;
+
+        if (pos.x < transform.position.x && isFacingRight)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            isFacingRight = false;
+        }
+        else if (pos.x > transform.position.x && !isFacingRight)
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            isFacingRight = true;
+        }
 
         transform.position = pos;
     }
