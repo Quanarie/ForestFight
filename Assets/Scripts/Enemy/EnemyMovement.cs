6167589a5352e542921aafc5b6668827e8157422
sppeeedd <qustaine@gmail.com>
2021-07-01 23:44:30 +0300

    Added enemy1 animator and idle animation

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -7,6 +7,15 @@ public class EnemyMovement : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private float speed;
 
+    private Vector3 previousPosition;
+    private Animator animator;
+
+    private void Start()
+    {
+        previousPosition = transform.position;
+        animator = GetComponent<Animator>();
+    }
+
     private void Update()
     {
         if (Vector3.Distance(player.position, transform.position) < player.gameObject.GetComponent<PlayerMovement>().UnhiddenDistance)
@@ -16,6 +25,26 @@ public class EnemyMovement : MonoBehaviour
             direction.y = player.position.y - transform.position.y;
 
             transform.Translate(direction.normalized * speed * Time.deltaTime);
+
+            if (transform.position.x < previousPosition.x && transform.localScale.x > 0)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+            else if (transform.position.x > previousPosition.x && transform.localScale.x < 0)
+            {
+                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            }
+
+            if (transform.position == previousPosition)
+            {
+                animator.SetBool("isRunning", false);
+            }
+            else
+            {
+                animator.SetBool("isRunning", true);
+            }
+
+            previousPosition = transform.position;
         }
     }
 
